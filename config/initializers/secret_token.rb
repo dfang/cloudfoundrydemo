bed75f318fc7c12198a30df018dae2776ba1dd9e
dfang <dfang@outlook.com>
2012-08-20 00:11:55 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tips::Application.config.secret_token = 'a75d0b9020e178d129daff557952d33bc518f1dd602223ca785eb6133576378d4bd9f91980efe2633faa71e188ce62beb088747b2e9a55dcd6a0369e0b604a8b'
